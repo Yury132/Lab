062b75390ae32523bdc1e7f29ea7dcb0b049490c
usynin.yury <usynin.yury@yandex.ru>
2019-10-21 17:01:24 +0300

    input

--- Lab_1/Program.cs
+++ Lab_1/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Lab_1
 {
@@ -6,7 +10,28 @@ namespace Lab_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Title = "Усынин Юрий ИУ5-35Б";
+
+            bool f1, f2, f3;
+            double a, b, c;
+            do
+            {
+                Console.Write("Введите коэф А: ");
+                f1 = double.TryParse(Console.ReadLine(), out a);
+                Console.Write("Введите коэф B: ");
+                f2 = double.TryParse(Console.ReadLine(), out b);
+                Console.Write("Введите коэф C: ");
+                f3 = double.TryParse(Console.ReadLine(), out c);
+
+                if (!f1 || !f2 || !f3)
+                {
+                    Console.Write("Необходимо ввести вещественные числа!");
+                }
+            } while (!f1 || !f2 || !f3);
+
+            Console.WriteLine(a.ToString());
+            Console.WriteLine(b.ToString());
+            Console.WriteLine(c.ToString());
         }
     }
 }
