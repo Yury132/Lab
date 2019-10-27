20c1544e64ddc55593305f6a351a1260db035a09
usynin.yury <usynin.yury@yandex.ru>
2019-10-27 10:36:26 +0300

    Circle

---
+++ Lab_2/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_2
+{
+    class Circle : GeometricalFigure, IPrint
+    {
+        public double radius { get; set; }
+        public Circle(double r)
+        {
+            this.radius = r;
+            this.Type = "Круг";
+        }
+        public override double Area()
+        {
+            return Math.PI * this.radius * this.radius;
+        }
+        public override string ToString()
+        {
+            return this.Type + " с радиусом: " + this.radius.ToString() + " имеет площадь: " + this.Area().ToString();
+        }
+        public void Print()
+        {
+            Console.WriteLine(this.ToString());
+        }
+    }
+}
