14210f29c101331bba68a5373a79b08f0ea03863
usynin.yury <usynin.yury@yandex.ru>
2019-10-27 09:14:29 +0300

    GeometricalFigure and IPrint

---
+++ Lab_2/GeometricalFigure.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_2
+{
+    abstract class GeometricalFigure
+    {
+        public string Type { get; protected set; }
+
+        public abstract double Area();
+        public override string ToString()
+        {
+            return this.Type + " площадью " + this.Area().ToString();
+        }
+    }
+}
