c23db1853b4b0d4f0ab75f3156c88a88cb289cd9
usynin.yury <usynin.yury@yandex.ru>
2019-11-20 12:32:54 +0300

    button_1

--- Lab_4/Form1.cs
+++ Lab_4/Form1.cs
@@ -7,14 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
 
 namespace Lab_4
 {
     public partial class Form1 : Form
     {
+        List<string> list = new List<string>();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "текстовые файлы|*.txt";
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                Stopwatch t = new Stopwatch(); 
+                t.Start();
+                string text = File.ReadAllText(fd.FileName);
+                char[] separators = new char[] { ' ', '.', ',', '!', '?', '/', '\t', '\n' };
+                string[] textArray = text.Split(separators);
+                foreach (string strTemp in textArray)
+                {
+                    string str = strTemp.Trim();
+                    if (!list.Contains(str)) list.Add(str);
+                }
+                t.Stop(); 
+                this.textBox1.Text = t.Elapsed.ToString(); 
+            }
+            else
+            {
+                MessageBox.Show("Необходимо выбрать файл!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
