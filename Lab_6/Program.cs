1f5b5d1b3a1ecd6221fd0801a4b611271ba497bc
usynin.yury <usynin.yury@yandex.ru>
2019-12-11 23:12:22 +0300

    Делегаты

--- Lab_6/Program.cs
+++ Lab_6/Program.cs
@@ -7,6 +7,7 @@ using System.Reflection;
 
 namespace Lab_6
 {
+
     class Program
     {
         public static bool GetPropertyAttribute(PropertyInfo checkType, Type attributeType, out object attribute)
@@ -23,14 +24,113 @@ namespace Lab_6
             return false;
         }
 
+        delegate int Compare(int p1, string p2); //делегат
 
+        static int CompareEq(int p1, string p2) //метод, соответствующий данному делегату
+        {
+            int p2i;
+            if (int.TryParse(p2, out p2i))
+            {
+                p2i = int.Parse(p2);
+                if (p1 == p2i) return 1;
+                else return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        static void Write(int n, Compare comparator) // метод, принимающий разработанный делегат, в качестве одного из входным параметров.
+        {
+            string str;
+            int res;
+
+            Console.Write("Введите строку для сравнения: ");
+            str = Console.ReadLine();
+            res = comparator(n, str);
+
+            if (res == 1)
+            {
+                Console.Write("Выражение верно.");
+            }
+            else if (res == 0)
+            {
+                Console.Write("Выражение неверно.");
+            }
+            else
+            {
+                Console.Write("Выражение некорректно.");
+            }
+        }
+
+        static void WriteF(int n, Func<int, string, int> comparator) //метод, принимающий обобщенный делегат Func< >
+        {
+            string str;
+            int res;
+
+            Console.Write("Введите строку для сравнения: ");
+            str = Console.ReadLine();
+            res = comparator(n, str);
+
+            if (res == 1)
+            {
+                Console.Write("Выражение верно.");
+            }
+            else if (res == 0)
+            {
+                Console.Write("Выражение неверно.");
+            }
+            else
+            {
+                Console.Write("Выражение некорректно.");
+            }
+        }
 
         static void Main(string[] args)
         {
+            Console.WriteLine("Часть 1: ДЕЛЕГАТЫ\n");
+            int i = 7;
 
+            Console.Write("Пример:\nЧисло для сравнения: {0}\nПередача метода в качестве параметра-делегата: \n\t", i);
+            Write(i, CompareEq); //вызов метода с параметром-делегатом
+            Console.Write("\nПередача лямбда-выражения в качестве параметра-делегата: \n\t");
+            Write(i,
+                (int p1, string p2) =>
+                {
+                    int p2i;
+                    if (int.TryParse(p2, out p2i))
+                    {
+                        p2i = int.Parse(p2);
+                        if (p1 == p2i) return 1;
+                        else return 0;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+                );
 
-
-
+            Console.Write("\nИспользование в качестве параметра обобщенного делегата:\nПередача метода в качестве параметра-делегата: \n\t");
+            WriteF(i, CompareEq); //вызов метода с параметром-делегатом
+            Console.Write("\nПередача лямбда-выражения в качестве параметра-делегата: \n\t");
+            WriteF(i,
+                (int p1, string p2) =>
+                {
+                    int p2i;
+                    if (int.TryParse(p2, out p2i))
+                    {
+                        p2i = int.Parse(p2);
+                        if (p1 == p2i) return 1;
+                        else return 0;
+                    }
+                    else
+                    {
+                        return -1;
+                    }
+                }
+                );
 
 
             Console.WriteLine("\nЧасть 2: РЕФЛЕКСИЯ\n");
